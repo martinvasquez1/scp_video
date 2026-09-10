2d08c229572ffbbb639ba95d630788b69e6ab93e
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-09-10 10:58:02 -0300

    Add pseudocode

---
+++ src/scp_video/pseudocode.py
@@ -0,0 +1,13 @@
+from manim import *
+
+class Pseudocode(Scene):
+    def construct(self):
+        pseudocode = Code(
+            "pseudocode.txt",
+            language="text",
+            background="rectangle",
+        )
+        
+        pseudocode.to_corner(UL)
+        self.play(Create(pseudocode))
+        self.wait(2)
