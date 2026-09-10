de50670c6500837e95724f863422d6d3c6f2f98c
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-09-10 10:47:11 -0300

    Add tn_sol

---
+++ src/scp_video/tn_sol.py
@@ -0,0 +1,37 @@
+from manim import *
+
+from utils.equations import show_equation
+
+class TNSolution(Scene):
+    display_time = 0.3
+
+    def construct(self):
+        pattern = MathTex(r"2^i T(n-i) + (2^i - 1)k_2")
+
+        self.play(FadeIn(pattern))
+        self.play(pattern.animate.to_corner(UL, buff=0.5))
+
+        # Solve i
+        solve_i = show_equation(
+            self,
+            [
+                r"T(n-i) = T(1)",
+                r"n-i = 1",
+                r"i = n - 1",
+            ],
+        )
+
+        self.play(solve_i.animate.to_corner(UR, buff=0.5))
+
+        # Solution
+        solution = show_equation(
+            self,
+            [
+                r"2^i T(n-i) + (2^i-1)k_2",
+                r"2^{n-1}T(n-(n-1)) + (2^{n-1}-1)k_2",
+                r"2^{n-1}T(1) + (2^{n-1}-1)k_2",
+                r"2^{n-1}k_1 + (2^{n-1}-1)k_2",
+            ],
+        )
+
+        self.wait(2)
